2de507049857e202c34584330ca47371deef16d7
B1029006 <B1029006@cgu.edu.tw>
2022-05-02 22:21:48 +0800

    main.c

--- lab2/main.c
+++ lab2/main.c
@@ -2,159 +2,123 @@
 #include <stdlib.h>
 #include <time.h>
 
-typedef struct lotto_record_t {
-    int lotto_num;
-    float lotto_receipt;
-    int emp_id;
-    char lotto_date[16];
-    char lotto_time[16];
-}lotto_record_t;
+typedef struct lotto_record {
+	int lotto_no[1];
+	int lotto_receipt[1];
+	int emp_id[1];
+	char lotto_date[32];
+	char lotto_time[32];
+} typelotto_record_t;
 
-int save_idfile() {
-    FILE* fid;
-    fid = fopen("operatid_name.bin", "wb+");
-    int id_num[1] = {0};
-    int idbuffer =0;
-    printf("\nPlese enter your id: ");
-    scanf("%d", &idbuffer);
-    return idbuffer;
-    id_num[0] = idbuffer;
-    fwrite(id_num, sizeof(int), 1, fid);
-    fclose(fid);
-}
-int rand_num_mod70(int lotto_array) {
-    lotto_array = rand() % 70;
-    while(lotto_array == 0) { 
-        lotto_array = rand() % 70;
-    } 
-    return lotto_array;
-}
-int rand_num_mod11(int lotto_array) {
-    lotto_array = rand() % 11;
-    while(lotto_array == 0) { 
-        lotto_array = rand() % 11;
-    } 
-    return lotto_array;
-}
-int get_rad_num(int lotto_array[7]) {
+int main(void) {
+	FILE* fp;
+	FILE* fp_count;
+	FILE* fp_operator;
+	FILE* fp_record;
+	int n, i, j, k, temp;
+	int num[5][7];
+	int count[] = {1};
+	int operator_id[1];
 
-    for(int j = 0; j < 7; j++) {
-        if(j < 6){
-            lotto_array[j] = rand_num_mod70(lotto_array[j]);
-        } else if (j = 6) {
-            lotto_array[6] = rand_num_mod11(lotto_array[6]);
-        }
-        //check lotto array[i] isn't 0;
-    }
-    return lotto_array[7];
-}
-int array_swap(int lotto_array[7]) {
-    int tmp = 0;
-    for(int j = 0; j < 6 ;j++) {
-        for(int arr_lotto = j; arr_lotto < 6; arr_lotto++) {
-            if (lotto_array[j] > lotto_array[arr_lotto]) {
-                tmp = lotto_array[j];
-                lotto_array[j] = lotto_array[arr_lotto];
-                lotto_array[arr_lotto] = tmp;
-            }
-        }
-    }
-    return lotto_array[7];
-}
-int array_rerandom(int lotto_array[7]) {
-    for (int j = 0; j < 6; j++) {
-        for(int z =0 ; z < 6; z++){
-            while ((lotto_array[j] == lotto_array[z] && j != z) || 
-                    lotto_array[j] == lotto_array[6]) {
-                lotto_array[j] = rand_num_mod70(lotto_array[j]);
-            }
-        }
-    }
-    return lotto_array[7];
-}
-int print_array (int lotto_array[7], FILE*fp) {
-    for(int j = 0 ; j < 7 ; j++) {
-        fprintf(fp, "%.2d ", lotto_array[j]);
-    }
-    fprintf(fp, "\n");
-}
-int print_last(int num_row, FILE*fp, int id_numx) {
-    for(int i = num_row+1; i < 6; i++) {
-        fprintf(fp,"[%d]: " , i);
-        for(int j = 0; j < 7; j++) {
-            fprintf(fp, "-- ");
-            j == 6 ? fprintf(fp,"\n") : 1;
-        }
-    }
-    fprintf(fp, "======== Op.%.5d =========\n", id_numx);
-    fprintf(fp, "======== csie@CGU =========\n");
-}
 
-int frecord (int sell_num, int id_numx, int row_num) {
-    FILE* femp;
-    time_t curtime = time(0);
-    lotto_record_t record;
-    record.lotto_num = sell_num;
-    record.lotto_receipt = ((sell_num * 50)+ (row_num*50)) * 1.1;
-    record.emp_id = id_numx;
-    strftime (record.lotto_date, 100, "%Y%m%d", localtime(&curtime));
-    strftime (record.lotto_time, 100, "%H:%M:%S", localtime(&curtime));
-    femp = fopen("records.bin", "ab");
-    fwrite(&record, sizeof(record), 1, femp);
-    fclose(femp);
-}
-int main() {
-    FILE* fp;
-    FILE* fr;
-    int sell_num[1]= {1}, sell_numpluse[1]= {1};
-        if((fr = fopen("a.bin", "r")) == NULL) {
-            fr = fopen("a.bin", "wb");
-            fwrite(sell_num, sizeof(int), 1, fr);
-        } else {
-            fr = fopen("a.bin", "rb");
-            fread(sell_num, sizeof(int), 1, fr);
-            fseek(fr, 0, SEEK_SET);
-            fclose(fr);
-            fr = fopen("a.bin", "wb+");
-            sell_numpluse[0] = sell_num[0] +1;
-            fwrite(sell_numpluse, sizeof(int), 1, fr);
-        }
-    int n =  snprintf(NULL, 0, "lotto[%05d].txt",sell_numpluse[0]);
-    char s1[n+1];
-    snprintf(s1, sizeof(s1), "lotto[%05d].txt",sell_numpluse[0]);
-    fp = fopen(s1, "w+"); 
-    fclose(fr);
-    int row_num = 0;
-    printf("\nplease enter your lotto row num: ");
-    scanf("%d", &row_num);
-    time_t curtime;
-    time(&curtime);
-    fprintf(fp, "======== lotto649 =========\n");
-    fprintf(fp, "=======+ No. %05d +=======\n= %.24s=\n",  sell_numpluse[0], ctime(&curtime));
-    srand((unsigned) curtime);
-    if (row_num <= 5) {
-        for (int i = 1; i <= row_num; i++) {
-            int lotto_array[8] = {0};
-            get_rad_num(lotto_array);
-            fprintf(fp, "[%d]: ", i);
-            lotto_array[8] = array_rerandom(lotto_array);
-            lotto_array[8] = array_swap(lotto_array);
-            print_array(lotto_array, fp); //fprintf3
-        }
-    } else {
-        for (int i = 1; i <= 5; i++) {
-            int lotto_array[8] = {0};
-            get_rad_num(lotto_array);
-            fprintf(fp, "[%d]: ", i);
-            lotto_array[8] = array_rerandom(lotto_array);
-            lotto_array[8] = array_swap(lotto_array);
-            print_array(lotto_array, fp); //fprintf3
-        }
-    }
-    int id_numx;
-    id_numx = save_idfile();
-    print_last(row_num, fp, id_numx); //fprintf4
-    fclose(fp);
-    frecord(sell_numpluse[0], id_numx), row_num);
-    return 0;
+	if((fp_count = fopen("lotto_count.bin", "rb")) != NULL) {
+		fread(count, sizeof(int), 1, fp_count);
+		fclose(fp_count);
+	} else {
+		fp_count = fopen("lotto_count.bin", "wb+");
+		fwrite(count, sizeof(int), 1, fp_count);
+		fclose(fp_count);
+	}
+
+	printf("歡迎光臨長庚樂透彩購買機台\n");
+
+	printf("請輸入操作人員ID:(0-5): ");
+	scanf("%d", &operator_id[0]);
+	fp_operator = fopen("operator_id.bin", "wb+");
+	fwrite(operator_id, sizeof(int), 1, fp_operator);
+	fclose(fp_operator);
+
+	printf("請問您要買幾組樂透彩 : ");
+	scanf("%d", &n);
+
+	char buffer_title[32];
+	snprintf(buffer_title, sizeof(char) * 32, "lotto[0000%i].txt", count[0]);
+	fp = fopen(buffer_title, "wb");
+	fprintf(fp, "========= lotto649 =========\n");
+	fprintf(fp, "========+ No.%05d +========\n", count[0]);
+
+	time_t curtime;
+	struct tm *info;
+	char buffer[80];
+	time(&curtime);
+	info = localtime(&curtime);
+	strftime(buffer, 80, "%a %b %d %X %Y", info);
+	fprintf(fp, "= %s =\n", buffer);
+
+	for(i = 0; i < 5; i++) {
+		for(j = 0; j < 6; j++) {
+			num[i][j] = rand() % 69 + 1;
+		}
+
+		for(j = 0; j < 6; j++) {
+			for(k = 0; k < 5 - j; k++) {
+				if(num[i][k] > num[i][k + 1]) {
+					temp = num[i][k];
+					num[i][k] = num[i][k + 1];
+					num[i][k + 1] = temp;
+				}
+			}
+		}
+	}
+	for(i = 0; i < 5; i++) {
+		fprintf(fp, "[%d]: ", i + 1);
+		num[i][6] = rand() % 10 + 1;
+		for(j = 0; j < 6; j++) {
+			if(i >= n) {
+				fprintf(fp, "-- ");
+			} else if(num[i][j] < 10) {
+				fprintf(fp, "0%d ", num[i][j]);
+			} else {
+				fprintf(fp, "%d ", num[i][j]);
+			}
+			if(num[i][6] == num[i][j]) {
+				num[i][6] = rand() % 10 + 1;
+			}
+		}
+
+		if(i >= n) {
+			fprintf(fp, "--\n");
+		} else if(num[i][6] == 10) {
+			fprintf(fp, "%d\n", num[i][6]);
+		} else {
+			fprintf(fp, "0%d\n", num[i][6]);
+		}
+	}
+
+	fprintf(fp, "========* Op.");
+	fp_operator = fopen("operator_id.bin", "rb");
+	fread(operator_id, sizeof(int), 1, fp_operator);
+	fclose(fp_operator);
+	fprintf(fp, "%05d", operator_id[0]);
+	fprintf(fp, " *========\n");
+	fprintf(fp, "========= csie@CGU =========\n");
+	fclose(fp);
+	printf("已為您購買的 %d 組樂透彩組合輸出至 lotto.txt\n", n);
+
+	count[0]++;
+	fp_count = fopen("lotto_count.bin", "wb+");
+	fwrite(count, sizeof(int), 1, fp_count);
+	fclose(fp_count);
+
+	typelotto_record_t record[1];
+	record[0].lotto_no[0] = count[0] - 1;
+	record[0].lotto_receipt[0] = n * 50 * 1.1;
+	record[0].emp_id[0] = operator_id[0];
+
+	strftime(record[0].lotto_date, 32, "%b %d %Y", info);
+	strftime(record[0].lotto_time, 32, "%X", info);
+	fp_record = fopen("records.bin", "ab");
+	fwrite(record, sizeof(typelotto_record_t), 1, fp_record);
+	fclose(fp_record);	
+	return 0;
 }
