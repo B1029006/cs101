db2b3afcb444840fe9a3ef201c9fac2d1df026e7
B1029006 <91193187+B1029006@users.noreply.github.com>
2022-05-31 15:02:41 +0800

    Update main.cpp

--- qz6/main.cpp
+++ qz6/main.cpp
@@ -1,49 +1,63 @@
-#include<iostream>
-#include<string>
+#include <iostream>
 using namespace std;
 
-class Clock{
-    public:
-    int hour,minute,second;
-    string meridian;
-    Clock(int H,int M,int S,string mer){
-        hour = H;
-        minute = M;
-        second = S;
-        meridian = mer;
-    }
-    display(){
-        cout << hour << ":" << minute << ":" << second << " " << meridian << endl;
-    }
+class Clock {
+	protected:
+		int hr;
+		int min;
+		int sec;
+	public:
+		string meridian;
+		Clock(int h, int m, int s, string mer) {
+			hr = h;
+			min = m;
+			sec = s;
+			meridian = mer;
+		}
+		void display() {
+			cout << hr << ":" << min << ":" << sec << " " << meridian << endl;
+		}
 };
 
-class StandardClock : Clock{
-    public:
-    StandardClock(int H,int M,int S,string mer):Clock(H,M,S,mer){
-        if(meridian=="AM") meridian = "上午";
-        else if(meridian=="PM") meridian = "下午";
-    }
-    display(){
-        cout << meridian << " " << hour << ":" << minute << ":" << second << endl;
-    }
+class StandardClock : Clock {
+	private:
+		string Chinese_meridian;
+	public:
+		StandardClock(int h, int m, int s, string mer) : Clock(h, m, s, mer) {
+			if(mer == "AM") {
+				Chinese_meridian = "上午";
+			} else {
+				Chinese_meridian = "下午";
+			}
+		}
+		void display() {
+			cout << Chinese_meridian << " " << hr << ":" << min << ":" << sec << endl;
+		}
+		
 };
 
-class MilitaryClock : Clock{
-    public:
-    MilitaryClock(int H,int M,int S,string mer):Clock(H,M,S,mer){
-        if(meridian=="PM") hour+=12;
-    }
-    display(){
-        cout <<  hour << ":" << minute << ":" << second;
-    }
+class MilitaryClock : Clock {
+	private:
+		int mc_h;
+	public:
+		MilitaryClock(int h, int m, int s, string mer) : Clock(h, m, s, mer) {
+			if(mer == "PM") {
+				mc_h = h + 12;
+			}
+		}
+		void display() {
+			cout << mc_h << ":" << min << ":" << sec << endl;
+		}
+		
 };
 
-int main(){
-    Clock cc(10,25,43,"AM");
-    StandardClock sc(10,25,43,"AM");
-    MilitaryClock mc(10,25,43,"PM");
-    cc.display();
-    sc.display();
-    mc.display();
-    return 0;
+int main(void) {
+	Clock cc(10, 25, 43, "AM");
+	StandardClock sc(10, 25, 43, "AM");
+	MilitaryClock mc(10, 25, 43, "PM");
+	cc.display();
+	sc.display();
+	mc.display();
+
+	return 0;
 }
